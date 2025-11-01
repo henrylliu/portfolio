d1d916df0768456cad7106b49b9bcd1c09088e22
Henry Liu <hel044@ucsd.edu>
2025-11-01 02:10:06 -0700

    Added year of project within project tab

--- global.js
+++ global.js
@@ -94,8 +94,11 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     article.innerHTML = `
       <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
-      <p>${project.description}</p>
-    `;
+      <div class="project-info">
+        <p class="project-description">${project.description}</p>
+        <p class="project-year">${project.year}</p>
+      </div>
+      `;
     containerElement.appendChild(article);
   });
   if (projects.length === 0) {
